aac2dbbaec63eb0db24c602ab4a6aa7edb64f73d
pavel m <paullus325@gmail.com>
2019-03-25 14:26:11 +0300

    init

---
+++ clean.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	path2 "path"
+
+	"strings"
+
+	"fmt"
+
+	"github.com/anacrolix/torrent/bencode"
+	"github.com/anacrolix/torrent/metainfo"
+)
+
+func main() {
+	dryRun := true
+	if len(os.Args) != 3 && len(os.Args) != 4 {
+		fmt.Println("First argument - download directory, second argument torrents directory, third - dry run by default true")
+	}
+	if len(os.Args) == 4 {
+		dryRun = false
+	}
+	files, err := ioutil.ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	torrents := make([]string, 0)
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), ".torrent") {
+			tnm, err := tname(path2.Join(os.Args[2], f.Name()))
+			if err != nil {
+				torrents = append(torrents, tnm)
+			} else {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	files, err = ioutil.ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		found := false
+		for i := range torrents {
+			if torrents[i] == f.Name() {
+				found = true
+				break
+			}
+		}
+		if !found {
+			if dryRun {
+				fmt.Printf("Orphan %s", f.Name())
+			} else {
+				fmt.Printf("Removed orphan %s", f.Name())
+			}
+		}
+	}
+}
+
+func tname(file string) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	mi, err := metainfo.Load(f)
+	if err != nil {
+		return "", err
+	}
+
+	var info metainfo.Info
+	err = bencode.Unmarshal(mi.InfoBytes, &info)
+	if err != nil {
+		return "", err
+	}
+	return info.Name, nil
+}
